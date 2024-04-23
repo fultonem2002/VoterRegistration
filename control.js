2204ce2243d702efeb1b242eb7addcbd280a4399
Ella <fultonem@appstate.edu>
2024-04-23 14:33:48 -0400

    Set up control and index files

--- control.js
+++ control.js
@@ -1,1 +1,23 @@
+new (class Control {
+    constructor() {
+        const root = d3.select('body').append('div')
+            .style('width', '120vw')
+            .style('height', '60vw');
 
+        // Change the file names below to fit:
+        // this.pieChart = new PieChart(this, root);
+        // this.barChart = new BarChart(this, root);
+        // this.mapView = new MapView(this, root);
+    }
+
+    Test(str) {
+        console.log(str);
+    }
+
+    // For the interaction part
+    clickMethod(Message,Color){
+        this.viewA.receiveInfo(Message,Color);
+        this.viewB.receiveInfo(Message,Color);
+        this.viewC.receiveInfo(Message,Color);
+    }
+})()
